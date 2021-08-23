79bd715e870b4561c2495af2e8e180eb2a7d1282
ashenafi03 <zaaf4travel@gmail.com>
2021-08-22 23:07:38 -0400

    mimisho requst

---
+++ Firstprojectaug/src/com/izaaf/gitdemo.java
@@ -0,0 +1,11 @@
+package com.izaaf;
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java world");
+
+	}
+
+}
